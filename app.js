e4fb361b3b9058672ad8ae4df68e586d0a0ee2f7
stephane boguhe <boguhestephane97@gmail.com>
2022-04-05 14:49:45 +0000

    Ajout de l'api
    -> Ajout des identifiants de l'API mailchimp
    -> Récupération et envoie des données au server

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const https = require("https");
 
 const app = express();
 dotenv.config();
 
 port = process.env.PORT;
+const listID = process.env.ID;
+const API_KEY = process.env.APIKey;
+const url = "https://us14.api.mailchimp.com/3.0/lists/" + listID;
 
 //module utilisé
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +17,38 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const mail = req.body.mail;
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+
+  const data = {
+    members: [
+      {
+        email_address: mail,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: prenom,
+          LNAME: nom,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const options = {
+    method: "POST",
+    auth: "stephane97:" + API_KEY,
+  };
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(port, () => {
