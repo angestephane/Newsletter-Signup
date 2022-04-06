47fd471701b08531e8b9293584525933447beb8c
stephane boguhe <boguhestephane97@gmail.com>
2022-04-06 12:18:52 +0000

    Création du modèle
    Création du modèle contact

---
+++ contact.js
@@ -0,0 +1,8 @@
+module.exports = class Contact {
+  constructor(nom, prenom, email, phone) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.email = email;
+    this.phone = phone;
+  }
+};
